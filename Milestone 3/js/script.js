61bf95e07e85c7d4ca745cd709124f3965712d88
tgiannotta <t.giannotta92@gmail.com>
2021-11-16 17:14:12 +0100

    End milestone 3

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -1,26 +1,26 @@
 const boxesContainer = document.querySelector('.container');
 
 
-for (let i = 1; i <=100; i++) {
+for (let i = 1; i <=1000; i++) {
     let contenuto;
 
     if ((i % 3 === 0) && (i % 5 === 0)){
-        contenuto = 'FizzBuzz';
+        contenuto = 'fizzbuzz';
         console.log(i);
         console.log(contenuto);
     }
     else if (i % 3 === 0) {
-        contenuto = 'Fizz';
+        contenuto = 'fizz';
     }
     else if (i % 5 === 0) {
-        contenuto = 'Buzz';
+        contenuto = 'buzz';
     }
     else {
         contenuto = i;
     }
         
     let newBox = `
-    <div class="box">${contenuto}</div>
+    <div class="box ${contenuto}">${contenuto}</div>
  `;
     boxesContainer.innerHTML += newBox;
     console.log(newBox);
